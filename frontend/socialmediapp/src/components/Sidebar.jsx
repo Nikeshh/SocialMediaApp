ee3a8153201c00bcceb970c438e5451581ef8e40
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-12 10:16:45 -0500

    SOC-6 Frontend Home Sidebar changes

--- frontend/socialmediapp/src/components/Sidebar.jsx
+++ frontend/socialmediapp/src/components/Sidebar.jsx
@@ -1,6 +1,13 @@
 import { IoIosArrowForward } from 'react-icons/io';
 
 const Sidebar = ({ closeToggle, user }) => {
+
+    const handleCloseSidebar = () => {
+        if(closeToggle) {
+            closeToggle(false);
+        }
+    }
+
     return(
         <div className="">
            <div className="">
@@ -8,6 +15,9 @@ const Sidebar = ({ closeToggle, user }) => {
            </div>
            {user && (
                <Link
+                to={`user-profile/${user._id}`}
+                className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+                onClick={handleCloseSidebar}
                >
                    <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
                    <p>{user.userName}</p>
