d37188b0e6654d8a3445236b518ecf627d7128e0
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-11 23:31:36 -0500

    SOC-6 Frontend Home changes

--- frontend/socialmediapp/src/container/home.jsx
+++ frontend/socialmediapp/src/container/home.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useRef, useState } from "react";
+import Sidebar from "../components/Sidebar";
+
 const Home = () => {
 
-    const userInfo = localStorage.getItem('user') !=='undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear(); 
+    const [user, setUser] = useState();
+    const scrollRef = useRef(null);
+
+    const userInfo = localStorage.getItem('user') !=='undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    
+    useEffect(() => {
+        // Todo: Fetch user query and set the user
+    }, []);
+
+    useEffect(() => {
+        scrollRef.current.scrollTo(0, 0);
+    });
 
     return (
         <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
             <div className="hidden md:flex h-screen flex-initial">
-            
+                <Sidebar user={user && user} />
             </div>
             <div className="">
 
