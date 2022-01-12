d48d903c15e7fdd6282ba5e6e188e231291f7215
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-11 23:25:31 -0500

    SOC-6 Frontend Home changes

--- frontend/socialmediapp/src/container/home.jsx
+++ frontend/socialmediapp/src/container/home.jsx
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const userInfo = localStorage.getItem('user') !=='undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear(); 
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+            <div className="hidden md:flex h-screen flex-initial">
+            
+            </div>
+            <div className="">
+
+            </div>
+            <div className="">
+
+            </div>
         </div>
     );
 };
