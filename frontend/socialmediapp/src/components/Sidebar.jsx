9fefe17abf8a11bfadd9aded1f9638c60a1b2529
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-12 08:21:43 -0500

    SOC-6 Frontend Home Sidebar structure changes

--- frontend/socialmediapp/src/components/Sidebar.jsx
+++ frontend/socialmediapp/src/components/Sidebar.jsx
@@ -1,7 +1,19 @@
+import { IoIosArrowForward } from 'react-icons/io';
+
 const Sidebar = ({ closeToggle, user }) => {
     return(
-        <div>
-            <h1>Sidebar</h1>
+        <div className="">
+           <div className="">
+
+           </div>
+           {user && (
+               <Link
+               >
+                   <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+                   <p>{user.userName}</p>
+                   <IoIosArrowForward />
+               </Link>
+           )}
         </div>
     );
 };
