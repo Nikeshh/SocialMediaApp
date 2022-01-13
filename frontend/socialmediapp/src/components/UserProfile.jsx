6437b8dc831476230afac69b3700fbb0816aa8ce
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-13 00:25:06 -0500

    SOC-9 Frontend User Profile changes

--- frontend/socialmediapp/src/components/UserProfile.jsx
+++ frontend/socialmediapp/src/components/UserProfile.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { GoogleLogout } from 'react-google-login';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { client } from '../client';
 import { userQuery } from '../utils/data';
 import Spinner from './Spinner';
+import { AiOutlineLogout } from 'react-icons/ai';
 
 const UserProfile = () => {
 
     const { userId } = useParams();
+    const navigate = useNavigate();
 
     const [user, setUser] = useState();
 
@@ -21,11 +24,18 @@ const UserProfile = () => {
         return <Spinner message="Loading Profile" />;
     }
 
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    const logout = () => {
+        localStorage.clear();
+        navigate('/login');
+    }
+
     return (
         <div className="relative pb-2 h-full justify-center items-center">
-            <div className="">
-                <div className="">
-                    <div className="">
+            <div className="flex flex-col pb-5">
+                <div className="relative flex flex-col mb-7">
+                    <div className="flex flex-col justify-center items-center">
                         <img
                             className="w-full h-370 2xl:h-510 shadow-lg object-cover"
                             src="https://source.unsplash.com/1600x900/?nature,photography,technology"
@@ -36,6 +46,28 @@ const UserProfile = () => {
                             src={user.image}
                             alt="user-pic"
                         />
+                    </div>
+                    <h1 className='font-bold text-3xl text-center mt-3'>
+                        {user.userName}
+                    </h1>
+                    <div className='absolute top-0 z-1 right-0 p-2'>
+                        {userId === User.googleId && (
+                            <GoogleLogout
+                                clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+                                render={(renderProps) => (
+                                    <button
+                                        type='button'
+                                        className='bg-white p-2 rounded-full cursor-pointer outline-none shadow-md'
+                                        onClick={renderProps.onClick}
+                                        disabled={renderProps.disabled}
+                                    >
+                                        <AiOutlineLogout color="red" fontSize={21} />
+                                    </button>
+                                )}
+                                onLogoutSuccess={logout}
+                                cookiePolicy="single_host_origin"
+                            />
+                        )}
                     </div>
                 </div>
                 <div>
