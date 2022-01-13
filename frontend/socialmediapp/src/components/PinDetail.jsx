ecf85c4d823faabcfdb2f47df2954fb2c13a2a53
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-13 18:36:38 -0500

    SOC-10 Frontend Pins changes

--- frontend/socialmediapp/src/components/PinDetail.jsx
+++ frontend/socialmediapp/src/components/PinDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from 'react-router-dom';
 import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
 import { client, urlFor } from '../client';
 import Spinner from './Spinner';
@@ -72,6 +72,18 @@ const PinDetail = ({ user }) => {
                             </h1>
                             <p className="mt-3">{pinDetail.about}</p>
                         </div>
+                        <Link
+                            to={`/user-profile/${pinDetail?.postedBy._id}`}
+                            className="flex gap-2 mt-5 items-center bg-white rounded-lg"
+                        >
+                            <img
+                                src={pinDetail?.postedBy.image}
+                                className="w-10 h-10 rounded-full"
+                                alt="user-profile"
+                            />
+                            <p className="font-bold">{pinDetail?.postedBy.userName}</p>
+                        </Link>
+                        {/* Comments come here */}
                     </div>
                 </div>
             )}
