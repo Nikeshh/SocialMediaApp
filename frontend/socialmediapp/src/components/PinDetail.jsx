2efd9bd2fe51351a09fa4b70b1f9f31bc0c60b6a
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-13 18:32:31 -0500

    SOC-10 Frontend Pins changes

--- frontend/socialmediapp/src/components/PinDetail.jsx
+++ frontend/socialmediapp/src/components/PinDetail.jsx
@@ -66,7 +66,12 @@ const PinDetail = ({ user }) => {
                                 {pinDetail.destination?.slice(8)}
                             </a>
                         </div>
-                        {/* Pin Detail Title goes here */}
+                        <div>
+                            <h1 className="text-4xl font-bold break-words mt-3">
+                                {pinDetail.title}
+                            </h1>
+                            <p className="mt-3">{pinDetail.about}</p>
+                        </div>
                     </div>
                 </div>
             )}
