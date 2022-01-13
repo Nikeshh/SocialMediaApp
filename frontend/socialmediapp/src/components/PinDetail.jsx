690eef92c95dadad4b0b0f542a7562169321541b
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-13 16:17:49 -0500

    SOC-10 Frontend Pins changes

--- frontend/socialmediapp/src/components/PinDetail.jsx
+++ frontend/socialmediapp/src/components/PinDetail.jsx
@@ -1,8 +1,57 @@
-const PinDetail = () => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import { client, urlFor } from '../client';
+import Spinner from './Spinner';
+
+const PinDetail = ({ user }) => {
+
+    const { pinId } = useParams();
+    const [pins, setPins] = useState();
+    const [pinDetail, setPinDetail] = useState();
+
+    const fetchPinDetails = () => {
+        const query = pinDetailQuery(pinId);
+        if(query) {
+            client
+                .fetch(`${query}`)
+                .then((data) => {
+                    setPinDetail(data[0]);
+                    if(data[0]) {
+                        const query1 = pinDetailMorePinQuery(data[0]);
+                        client
+                            .fetch(query1)
+                            .then((res) => {
+                                setPins(res);
+                            });
+                    }
+                });
+        }
+    }
+
+    useEffect(() => {
+        fetchPinDetails();
+    }, [pinId]);
+
+    if(!pinDetail) {
+        return (
+            <Spinner message="Showing pin" />
+        );
+    }
+
     return (
-        <div>
-            <h1>PinDetail</h1>
-        </div>
+        <>
+            {pinDetail && (
+                <div>
+                    <h1>Pin Detail comes here</h1>
+                </div>
+            )}
+            {pins?.length > 0 && (
+                <h2 className="text-center font-bold text-2xl mt-8 mb-4">
+                    More like this
+                </h2>
+            )}
+        </>
     );
 }
 
