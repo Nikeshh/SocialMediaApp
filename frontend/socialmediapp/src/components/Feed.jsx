2dbf84ff78efc0730e518653b4bddf8a212f190e
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-14 08:24:32 -0500

    SOC-11 Frontend Feed changes

--- frontend/socialmediapp/src/components/Feed.jsx
+++ frontend/socialmediapp/src/components/Feed.jsx
@@ -1,8 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+import { feedQuery, searchQuery } from '../utils/data';
+
 const Feed = () => {
+
+    const [pins, setPins] = useState();
+    const { categoryId } = useParams();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (categoryId) {
+            setLoading(true);
+            const query = searchQuery(categoryId);
+            client
+                .fetch(query)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                });
+        } else {
+            setLoading(true);
+            client
+                .fetch(feedQuery)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                });
+        }
+    }, [categoryId]);
+
+    const ideaName = categoryId || 'new';
+    if(loading) {
+        return (
+            <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+        );
+    }
+
     return (
-        <h1>
-            Feed Section
-        </h1>
+        <div>
+            {pins && (
+                <MasonryLayout pins={pins} />
+            )}
+        </div>
     );
 }
 
