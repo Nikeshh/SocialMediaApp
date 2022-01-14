c02dc8ec1069f4058186f3f24cc486a1cb3b1ec8
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-14 08:36:21 -0500

    SOC-10 Frontend Navbar changes

--- frontend/socialmediapp/src/components/Navbar.jsx
+++ frontend/socialmediapp/src/components/Navbar.jsx
@@ -1,5 +1,19 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Navbar = ({ searchTerm, setSearchTerm, user }) => {
 
+    const navigate = useNavigate();
+
+    if(user) {
+        return (
+            <div>
+                Navbar with given user
+            </div>
+        );
+    }
+
+    return null;
 };
 
 export default Navbar;
--- frontend/socialmediapp/src/components/index.js
+++ frontend/socialmediapp/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Sidebar } from './Sidebar';
 export { default as PinDetail } from './PinDetail';
 export { default as Feed } from './Feed';
+export { default as Navbar } from './Navbar';
