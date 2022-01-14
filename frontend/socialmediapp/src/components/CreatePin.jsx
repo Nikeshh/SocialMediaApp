966ec41a036a42962423a223a07aeb80ff83283a
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-01-14 11:07:45 -0500

    SOC-10 Frontend pin creation changes

--- frontend/socialmediapp/src/components/CreatePin.jsx
+++ frontend/socialmediapp/src/components/CreatePin.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
 import Spinner from "./Spinner";
 
 const CreatePin = ({ user }) => {
@@ -6,6 +8,11 @@ const CreatePin = ({ user }) => {
     const [fields, setFields] = useState();
     const [loading, setLoading] = useState(false);
     const [wrongImageType, setWrongImageType] = useState(false);
+    const [imageAsset, setImageAsset] = useState();
+
+    const uploadImage = (e) => {
+
+    };
 
     return (
         <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
@@ -23,6 +30,43 @@ const CreatePin = ({ user }) => {
                                 <p>It&apos;s wrong file type.</p>
                             )
                         }
+                        {!imageAsset ? (
+                            // eslint-disable-next-line jsx-a11y/label-has-associated-control
+                            <label>
+                                <div className="flex flex-col items-center justify-center h-full">
+                                    <div className="flex flex-col justify-center items-center">
+                                        <p className="font-bold text-2xl">
+                                            <AiOutlineCloudUpload />
+                                        </p>
+                                        <p className="text-lg">Click to upload</p>
+                                    </div>
+                                    <p className="mt-32 text-gray-400">
+                                        Recommendation: Use high-quality JPG, JPEG, SVG, PNG, GIF or TIFF less than 20MB   
+                                    </p>
+                                </div>
+                                <input
+                                    type="file"
+                                    name="upload-image"
+                                    onChange={uploadImage}
+                                    className="w-0 h-0"
+                                />
+                            </label>
+                        ) : (
+                            <div className="relative h-full">
+                                <img
+                                    src={imageAsset?.url}
+                                    alt="uploaded-pic"
+                                    className="h-full w-full"
+                                />
+                                <button
+                                    type="button"
+                                    className="absolute bottom-3 right-3 p-3 rounded-full bg-white text-xl cursor-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out"
+                                    onClick={() => setImageAsset(null)}
+                                >
+                                    <MdDelete />
+                                </button>
+                            </div>
+                        )}
                     </div>
                 </div>
                 <div>
